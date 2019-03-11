7038b6411b270b5caabcc09c61ba0d7df3ae918d
z3u2 <ilyassnasr@gmail.com>
2019-03-11 02:37:29 +0100

    Add support of single reference in extractFields
    Add support of single reference in extractFields, and refactor for clearer
    recursivity

--- src/normalize.js
+++ src/normalize.js
@@ -1,5 +1,19 @@
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
 
+/**
+ * Tests if field is an object
+ *
+ *
+ * @param {any} field - Field to test
+ * @returns {boolean} - True if `field` is an object, False if null or not an object
+ */
+const isObject = field => {
+  if (field === null) {
+    return false
+  }
+  return typeof field === 'object'
+}
+
 const extractFields = async (
   apiURL,
   store,
@@ -12,8 +26,53 @@ const extractFields = async (
 ) => {
   for (const key of Object.keys(item)) {
     const field = item[key]
-    if (Array.isArray(field)) {
-      // add recursion to fetch nested strapi references
+    // image fields have a mime property among other
+    // maybe should find a better test
+    if (field !== null && field.hasOwnProperty('mime')) {
+      let fileNodeID
+      // using field on the cache key for multiple image field
+      const mediaDataCacheKey = `strapi-media-${item.id}-${key}`
+      const cacheMediaData = await cache.get(mediaDataCacheKey)
+
+      // If we have cached media data and it wasn't modified, reuse
+      // previously created file node to not try to redownload
+      if (cacheMediaData && field.updatedAt === cacheMediaData.updatedAt) {
+        fileNodeID = cacheMediaData.fileNodeID
+        touchNode(cacheMediaData.fileNodeID)
+      }
+
+      // If we don't have cached data, download the file
+      if (!fileNodeID) {
+        try {
+          // full media url
+          const source_url = apiURL + field.url
+          const fileNode = await createRemoteFileNode({
+            url: source_url,
+            store,
+            cache,
+            createNode,
+            createNodeId,
+            auth,
+          })
+
+          // If we don't have cached data, download the file
+          if (fileNode) {
+            fileNodeID = fileNode.id
+
+            await cache.set(mediaDataCacheKey, {
+              fileNodeID,
+              modified: field.updatedAt,
+            })
+          }
+        } catch (e) {
+          // Ignore
+        }
+      }
+      if (fileNodeID) {
+        item[`${key}___NODE`] = fileNodeID
+      }
+    } else if (Array.isArray(field)) {
+      // add recursion to fetch nested strapi references (array)
       await Promise.all(
         field.map(async f =>
           extractFields(
@@ -28,53 +87,18 @@ const extractFields = async (
           )
         )
       )
-    } else {
-      // image fields have a mime property among other
-      // maybe should find a better test
-      if (field !== null && field.hasOwnProperty('mime')) {
-        let fileNodeID
-        // using field on the cache key for multiple image field
-        const mediaDataCacheKey = `strapi-media-${item.id}-${key}`
-        const cacheMediaData = await cache.get(mediaDataCacheKey)
-
-        // If we have cached media data and it wasn't modified, reuse
-        // previously created file node to not try to redownload
-        if (cacheMediaData && field.updatedAt === cacheMediaData.updatedAt) {
-          fileNodeID = cacheMediaData.fileNodeID
-          touchNode(cacheMediaData.fileNodeID)
-        }
-
-        // If we don't have cached data, download the file
-        if (!fileNodeID) {
-          try {
-            // full media url
-            const source_url = apiURL + field.url
-            const fileNode = await createRemoteFileNode({
-              url: source_url,
-              store,
-              cache,
-              createNode,
-              createNodeId,
-              auth,
-            })
-
-            // If we don't have cached data, download the file
-            if (fileNode) {
-              fileNodeID = fileNode.id
-
-              await cache.set(mediaDataCacheKey, {
-                fileNodeID,
-                modified: field.updatedAt,
-              })
-            }
-          } catch (e) {
-            // Ignore
-          }
-        }
-        if (fileNodeID) {
-          item[`${key}___NODE`] = fileNodeID
-        }
-      }
+    } else if (isObject(field)) {
+      // add recursion to fetch nested strapi references (single reference)
+      await extractFields(
+        apiURL,
+        store,
+        cache,
+        createNode,
+        createNodeId,
+        touchNode,
+        auth,
+        field
+      )
     }
   }
 }
