33e926bea45bb88248a20f691194972b6c5e6553
Billy <bensingerbilly@gmail.com>
2020-03-30 10:08:24 -0400

    Refactoring code to use Array.isArray method, and removing unused parameter.
    Signed-off-by: Billy <bensingerbilly@gmail.com>

--- src/fetch.js
+++ src/fetch.js
@@ -29,7 +29,7 @@ module.exports = async ({
   const documents = await axios(apiEndpoint, fetchRequestConfig)
 
   // Make sure response is an array for single type instances
-  const response = documents.data.length ? documents.data : [ documents.data ]
+  const response = Array.isArray(documents.data) ? documents.data : [ documents.data ]
 
   // Map and clean data.
   return response.map(item => clean(item))
@@ -41,7 +41,7 @@ module.exports = async ({
  * @param {object} item - Entry needing clean
  * @returns {object} output - Object cleaned
  */
-const clean = (item, singleType) => {
+const clean = (item) => {
   forEach(item, (value, key) => {
     if (startsWith(key, `__`)) {
       delete item[key]
