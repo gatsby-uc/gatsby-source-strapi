85d137bd6b0778e79bcca5d952cf5e101dfa8aaa
denisgoryaynov <denis.gor27@gmail.com>
2020-05-14 17:56:36 +0200

    Support strapi extra image fields on single media
    Signed-off-by: denisgoryaynov <denis.gor27@gmail.com>

--- src/normalize.js
+++ src/normalize.js
@@ -1,105 +1,104 @@
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
 
+const isImage = (obj) => {
+	// image fields have a mime property among other
+	// maybe should find a better test
+	return obj && Object.prototype.hasOwnProperty.call(obj, 'mime')
+}
+
+const isObject = (obj) => {
+	return obj && Object.prototype.toString.apply(obj) === '[object Object]'
+}
+
 const extractFields = async (
-  apiURL,
-  store,
-  cache,
-  createNode,
-  createNodeId,
-  touchNode,
-  auth,
-  item,
-  key = 'localFile'
+	apiURL,
+	store,
+	cache,
+	createNode,
+	createNodeId,
+	touchNode,
+	auth,
+	item,
 ) => {
-  // image fields have a mime property among other
-  // maybe should find a better test
-  if (item && item.hasOwnProperty('mime')) {
-    let fileNodeID
-    // using field on the cache key for multiple image field
-    const mediaDataCacheKey = `strapi-media-${item.id}-${key}`
-    const cacheMediaData = await cache.get(mediaDataCacheKey)
-    const itemUpdatedAt = item.updatedAt || item.updated_at
+	if (isImage(item)) {
+		let fileNodeID
+		// using field on the cache key for multiple image field
+		const mediaDataCacheKey = `strapi-media-${item.id}`
+		const cacheMediaData = await cache.get(mediaDataCacheKey)
+		const itemUpdatedAt = item.updatedAt || item.updated_at
 
-    // If we have cached media data and it wasn't modified, reuse
-    // previously created file node to not try to redownload
-    if (cacheMediaData && itemUpdatedAt === cacheMediaData.updatedAt) {
-      fileNodeID = cacheMediaData.fileNodeID
-      touchNode({ nodeId: cacheMediaData.fileNodeID })
-    }
+		// If we have cached media data and it wasn't modified, reuse
+		// previously created file node to not try to redownload
+		if (cacheMediaData && itemUpdatedAt === cacheMediaData.updatedAt) {
+			fileNodeID = cacheMediaData.fileNodeID
+			touchNode({ nodeId: cacheMediaData.fileNodeID })
+		}
 
-    // If we don't have cached data, download the file
-    if (!fileNodeID) {
-      try {
-        // full media url
-        const source_url = `${item.url.startsWith('http') ? '' : apiURL}${
-          item.url
-        }`
-        const fileNode = await createRemoteFileNode({
-          url: source_url,
-          store,
-          cache,
-          createNode,
-          createNodeId,
-          auth,
-        })
+		// If we don't have cached data, download the file
+		if (!fileNodeID) {
+			try {
+				// full media url
+				const source_url = `${item.url.startsWith('http') ? '' : apiURL}${
+					item.url
+				}`
+				const fileNode = await createRemoteFileNode({
+					url: source_url,
+					store,
+					cache,
+					createNode,
+					createNodeId,
+					auth,
+				})
 
-        // If we don't have cached data, download the file
-        if (fileNode) {
-          fileNodeID = fileNode.id
+				// If we don't have cached data, download the file
+				if (fileNode) {
+					fileNodeID = fileNode.id
 
-          await cache.set(mediaDataCacheKey, {
-            fileNodeID,
-            updatedAt: itemUpdatedAt,
-          })
-        }
-      } catch (e) {
-        // Ignore
-      }
-    }
+					await cache.set(mediaDataCacheKey, {
+						fileNodeID,
+						updatedAt: itemUpdatedAt,
+					})
+				}
+			} catch (e) {
+				// Ignore
+			}
+		}
 
-    if (fileNodeID) {
-      if (key !== 'localFile') {
-        return fileNodeID
-      }
+		if (fileNodeID) {
+			item.localFile___NODE = fileNodeID
+		}
+	}
 
-      item.localFile___NODE = fileNodeID
-    }
-  } else if (Array.isArray(item)) {
-    await Promise.all(
-      item.map(async f =>
-        extractFields(
-          apiURL,
-          store,
-          cache,
-          createNode,
-          createNodeId,
-          touchNode,
-          auth,
-          f
-        )
-      )
-    )
-  } else if (item && typeof item === 'object') {
-    for (const key of Object.keys(item)) {
-      const field = item[key]
+	// keep looping for extra fields such as formats
+	if (Array.isArray(item)) {
+		for (const key of item) {
+			await extractFields(
+				apiURL,
+				store,
+				cache,
+				createNode,
+				createNodeId,
+				touchNode,
+				auth,
+				key
+			)
+		}
+	} else if (isObject(item)) {
+		for (const key of Object.keys(item)) {
+			const field = item[key]
 
-      const fileNodeID = await extractFields(
-        apiURL,
-        store,
-        cache,
-        createNode,
-        createNodeId,
-        touchNode,
-        auth,
-        field,
-        key
-      )
-
-      if (fileNodeID) {
-        item[`${key}___NODE`] = fileNodeID
-      }
-    }
-  }
+			await extractFields(
+				apiURL,
+				store,
+				cache,
+				createNode,
+				createNodeId,
+				touchNode,
+				auth,
+				field
+			)
+		}
+	}
 }
 
 // Downloads media from image type fields
