9fb72a18b1e55e149dfa059f5c0caaea5ea68cd7
soupette <cyril@strapi.io>
2022-01-25 17:13:55 +0100

    Delete old nodes
    Signed-off-by: soupette <cyril@strapi.io>

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -11,9 +11,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
  */
-import { capitalize } from 'lodash';
+import _ from 'lodash';
 import { fetchStrapiContentTypes, fetchEntities, fetchEntity } from './fetch';
-import { getEndpoints } from './helpers';
+import { buildMapFromNodes, buildNodesToRemoveMap, getEndpoints } from './helpers';
 import { downloadMediaFiles, createNodes } from './normalize';
 
 const CACHE_CONTENT_TYPES = `strapi-content-types`;
@@ -40,7 +40,7 @@ exports.sourceNodes = async (
   await cache.set(CACHE_CONTENT_TYPES, contentTypes);
   await cache.set(CACHE_COMPONENTS, components);
 
-  const { touchNode } = actions;
+  const { deleteNode, touchNode } = actions;
 
   const ctx = {
     strapiConfig: pluginOptions,
@@ -57,10 +57,15 @@ exports.sourceNodes = async (
   };
 
   const existingNodes = getNodes().filter((n) => n.internal.owner === `gatsby-source-strapi`);
+
   existingNodes.forEach((n) => touchNode(n));
 
   const endpoints = getEndpoints(pluginOptions, schemas);
 
+  // TODO caching
+  // This works great to retrieve all the deleted entities but maybe we should request the api
+  // another time to get the updated entries.
+  // Though we might have an issue when a relation is updated...
   const data = await Promise.all(
     endpoints.map(({ kind, ...config }) => {
       if (kind === 'singleType') {
@@ -71,10 +76,31 @@ exports.sourceNodes = async (
     })
   );
 
+  // Build a map of all nodes with the gatsby id and the strapi_id
+  const existingNodesMap = buildMapFromNodes(existingNodes);
+
+  // Build a map of all nodes that should be removed
+  // This should also delete all the created nodes for markdown, relations, dz...
+  const nodesToRemoveMap = buildNodesToRemoveMap(existingNodesMap, endpoints, data);
+
+  // Delete all nodes that should be deleted
+  Object.entries(nodesToRemoveMap).forEach(([nodeName, nodesToDelete]) => {
+    if (nodesToDelete.length) {
+      reporter.info(`Strapi: ${nodeName} deleting ${nodesToDelete.length}`);
+
+      nodesToDelete.forEach(({ id }) => {
+        const node = getNode(id);
+
+        touchNode(node);
+        deleteNode(node);
+      });
+    }
+  });
+
   for (let i = 0; i < endpoints.length; i++) {
     const { singularName, uid } = endpoints[i];
 
-    const nodeType = `Strapi${capitalize(singularName)}`;
+    const nodeType = `Strapi${_.capitalize(singularName)}`;
 
     await downloadMediaFiles(data[i], ctx, uid);
 
--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,68 @@
+import _ from 'lodash';
+
+const buildMapFromNodes = (nodes) => {
+  return nodes.reduce((acc, current) => {
+    const { internal, strapi_id, id } = current;
+
+    if (internal?.type && strapi_id && id) {
+      if (acc[internal.type]) {
+        acc[internal.type] = [
+          ...acc[internal.type],
+          {
+            strapi_id,
+            id,
+          },
+        ];
+      } else {
+        acc[internal.type] = [
+          {
+            strapi_id,
+            id,
+          },
+        ];
+      }
+    }
+
+    return acc;
+  }, {});
+};
+
+const buildMapFromData = (endpoints, data) => {
+  const map = {};
+
+  for (let i = 0; i < endpoints.length; i++) {
+    const { singularName } = endpoints[i];
+
+    const nodeType = `Strapi${_.capitalize(singularName)}`;
+
+    for (let entity of data[i]) {
+      if (map[nodeType]) {
+        map[nodeType] = [...map[nodeType], { strapi_id: entity.id }];
+      } else {
+        map[nodeType] = [{ strapi_id: entity.id }];
+      }
+    }
+  }
+
+  return map;
+};
+
+const buildNodesToRemoveMap = (existingNodesMap, endpoints, data) => {
+  const newNodes = buildMapFromData(endpoints, data);
+
+  const toRemoveMap = Object.entries(existingNodesMap).reduce((acc, [name, value]) => {
+    const currentNodes = newNodes[name] || [];
+
+    acc[name] = value.filter((j) => {
+      return currentNodes.findIndex((k) => k.strapi_id === j.strapi_id) === -1;
+    });
+
+    return acc;
+  }, {});
+
+  return toRemoveMap;
+};
+
 const getContentTypeSchema = (schemas, ctUID) => {
   const currentContentTypeSchema = schemas.find(({ uid }) => uid === ctUID);
 
@@ -48,4 +113,10 @@ const getEndpoints = ({ collectionTypes, singleTypes }, schemas) => {
   return endpoints;
 };
 
-export { getContentTypeSchema, getEndpoints };
+export {
+  buildMapFromNodes,
+  buildMapFromData,
+  buildNodesToRemoveMap,
+  getContentTypeSchema,
+  getEndpoints,
+};
