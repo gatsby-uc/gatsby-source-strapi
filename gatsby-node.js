b5f6d2d37c672557d61b64379c0957a3b799acbb
Kayla Gordon <kayla.m.gordon@gmail.com>
2022-06-27 18:20:42 +0300

    feat: fetch data for multiple configs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var _getIterator2 = require('babel-runtime/core-js/get-iterator');
+
+var _getIterator3 = _interopRequireDefault(_getIterator2);
+
 var _regenerator = require('babel-runtime/regenerator');
 
 var _regenerator2 = _interopRequireDefault(_regenerator);
@@ -46,135 +50,196 @@ exports.sourceNodes = function () {
         reporter = _ref2.reporter,
         getNode = _ref2.getNode,
         getNodes = _ref2.getNodes;
-    var _ref3$apiURL = _ref3.apiURL,
-        apiURL = _ref3$apiURL === undefined ? 'http://localhost:1337' : _ref3$apiURL,
-        _ref3$contentTypes = _ref3.contentTypes,
-        contentTypes = _ref3$contentTypes === undefined ? [] : _ref3$contentTypes,
-        _ref3$singleTypes = _ref3.singleTypes,
-        singleTypes = _ref3$singleTypes === undefined ? [] : _ref3$singleTypes,
-        _ref3$loginData = _ref3.loginData,
-        loginData = _ref3$loginData === undefined ? {} : _ref3$loginData,
-        _ref3$queryLimit = _ref3.queryLimit,
-        queryLimit = _ref3$queryLimit === undefined ? 100 : _ref3$queryLimit;
-    var createNode, deleteNode, touchNode, jwtToken, fetchActivity, contentTypePromises, singleTypePromises, entities, newNodes, existingNodes, diff;
-    return _regenerator2.default.wrap(function _callee$(_context) {
+    var _ref3$sources = _ref3.sources,
+        sources = _ref3$sources === undefined ? {} : _ref3$sources;
+
+    var createNode, deleteNode, touchNode, _loop, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, source;
+
+    return _regenerator2.default.wrap(function _callee$(_context2) {
       while (1) {
-        switch (_context.prev = _context.next) {
+        switch (_context2.prev = _context2.next) {
           case 0:
             createNode = actions.createNode, deleteNode = actions.deleteNode, touchNode = actions.touchNode;
+            _loop = /*#__PURE__*/_regenerator2.default.mark(function _loop(source) {
+              var apiURL, contentTypes, singleTypes, loginData, queryLimit, jwtToken, fetchActivity, contentTypePromises, singleTypePromises, entities, newNodes, existingNodes, diff;
+              return _regenerator2.default.wrap(function _loop$(_context) {
+                while (1) {
+                  switch (_context.prev = _context.next) {
+                    case 0:
+                      apiURL = source.apiURL, contentTypes = source.contentTypes, singleTypes = source.singleTypes, loginData = source.loginData, queryLimit = source.queryLimit;
 
-            // Authentication function
+                      // Authentication function
 
-            _context.next = 3;
-            return (0, _authentication2.default)({ loginData: loginData, reporter: reporter, apiURL: apiURL });
+                      _context.next = 3;
+                      return (0, _authentication2.default)({ loginData: loginData, reporter: reporter, apiURL: apiURL });
 
-          case 3:
-            jwtToken = _context.sent;
+                    case 3:
+                      jwtToken = _context.sent;
 
 
-            // Start activity, Strapi data fetching
-            fetchActivity = reporter.activityTimer('Fetched Strapi Data');
+                      // Start activity, Strapi data fetching
+                      fetchActivity = reporter.activityTimer('Fetched Strapi Data');
 
-            fetchActivity.start();
+                      fetchActivity.start();
 
-            // Generate a list of promises based on the `contentTypes` option.
-            contentTypePromises = contentTypes.map(function (contentType) {
-              return (0, _fetch2.default)({
-                apiURL: apiURL,
-                contentType: contentType,
-                jwtToken: jwtToken,
-                queryLimit: queryLimit,
-                reporter: reporter
-              });
+                      // Generate a list of promises based on the `contentTypes` option.
+                      contentTypePromises = contentTypes.map(function (contentType) {
+                        return (0, _fetch2.default)({
+                          apiURL: apiURL,
+                          contentType: contentType,
+                          jwtToken: jwtToken,
+                          queryLimit: queryLimit,
+                          reporter: reporter
+                        });
+                      });
+
+                      // Generate a list of promises based on the `singleTypes` option.
+
+                      singleTypePromises = singleTypes.map(function (singleType) {
+                        return (0, _fetch2.default)({
+                          apiURL: apiURL,
+                          singleType: singleType,
+                          jwtToken: jwtToken,
+                          queryLimit: queryLimit,
+                          reporter: reporter
+                        });
+                      });
+
+                      // Execute the promises
+
+                      _context.next = 10;
+                      return _promise2.default.all([].concat((0, _toConsumableArray3.default)(contentTypePromises), (0, _toConsumableArray3.default)(singleTypePromises)));
+
+                    case 10:
+                      entities = _context.sent;
+                      _context.next = 13;
+                      return _normalize2.default.downloadMediaFiles({
+                        entities: entities,
+                        apiURL: apiURL,
+                        store: store,
+                        cache: cache,
+                        createNode: createNode,
+                        touchNode: touchNode,
+                        jwtToken: jwtToken
+                      });
+
+                    case 13:
+                      entities = _context.sent;
+
+
+                      // new created nodes
+                      newNodes = [];
+
+                      // Fetch existing strapi nodes
+
+                      existingNodes = getNodes().filter(function (n) {
+                        return n.internal.owner === 'gatsby-source-strapi';
+                      });
+
+                      // Touch each one of them
+
+                      existingNodes.forEach(function (n) {
+                        touchNode({ nodeId: n.id });
+                      });
+
+                      // Merge single and content types and retrieve create nodes
+                      contentTypes.concat(singleTypes).forEach(function (type, i) {
+                        var items = entities[i];
+                        var name = (0, _lodash.isObject)(type) ? type.name : type;
+                        items.forEach(function (item, i) {
+                          var node = (0, _nodes.Node)((0, _lodash.capitalize)(name), item);
+                          // Adding new created nodes in an Array
+                          newNodes.push(node);
+                          // Create nodes
+                          createNode(node);
+                        });
+                      });
+
+                      // Make a diff array between existing nodes and new ones
+                      diff = existingNodes.filter(function (_ref4) {
+                        var id1 = _ref4.id;
+                        return !newNodes.some(function (_ref5) {
+                          var id2 = _ref5.id;
+                          return id2 === id1;
+                        });
+                      });
+
+                      // Delete diff nodes
+
+                      diff.forEach(function (data) {
+                        deleteNode({ node: getNode(data.id) });
+                      });
+                      fetchActivity.end();
+
+                      reporter.info('Finished fetching data from Strapi');
+
+                    case 22:
+                    case 'end':
+                      return _context.stop();
+                  }
+                }
+              }, _loop, undefined);
             });
+            _iteratorNormalCompletion = true;
+            _didIteratorError = false;
+            _iteratorError = undefined;
+            _context2.prev = 5;
+            _iterator = (0, _getIterator3.default)(sources);
 
-            // Generate a list of promises based on the `singleTypes` option.
+          case 7:
+            if (_iteratorNormalCompletion = (_step = _iterator.next()).done) {
+              _context2.next = 13;
+              break;
+            }
 
-            singleTypePromises = singleTypes.map(function (singleType) {
-              return (0, _fetch2.default)({
-                apiURL: apiURL,
-                singleType: singleType,
-                jwtToken: jwtToken,
-                queryLimit: queryLimit,
-                reporter: reporter
-              });
-            });
-
-            // Execute the promises
-
-            _context.next = 10;
-            return _promise2.default.all([].concat((0, _toConsumableArray3.default)(contentTypePromises), (0, _toConsumableArray3.default)(singleTypePromises)));
+            source = _step.value;
+            return _context2.delegateYield(_loop(source), 't0', 10);
 
           case 10:
-            entities = _context.sent;
-            _context.next = 13;
-            return _normalize2.default.downloadMediaFiles({
-              entities: entities,
-              apiURL: apiURL,
-              store: store,
-              cache: cache,
-              createNode: createNode,
-              touchNode: touchNode,
-              jwtToken: jwtToken
-            });
+            _iteratorNormalCompletion = true;
+            _context2.next = 7;
+            break;
 
           case 13:
-            entities = _context.sent;
+            _context2.next = 19;
+            break;
 
+          case 15:
+            _context2.prev = 15;
+            _context2.t1 = _context2['catch'](5);
+            _didIteratorError = true;
+            _iteratorError = _context2.t1;
 
-            // new created nodes
-            newNodes = [];
+          case 19:
+            _context2.prev = 19;
+            _context2.prev = 20;
 
-            // Fetch existing strapi nodes
+            if (!_iteratorNormalCompletion && _iterator.return) {
+              _iterator.return();
+            }
 
-            existingNodes = getNodes().filter(function (n) {
-              return n.internal.owner === 'gatsby-source-strapi';
-            });
+          case 22:
+            _context2.prev = 22;
 
-            // Touch each one of them
+            if (!_didIteratorError) {
+              _context2.next = 25;
+              break;
+            }
 
-            existingNodes.forEach(function (n) {
-              touchNode({ nodeId: n.id });
-            });
+            throw _iteratorError;
 
-            // Merge single and content types and retrieve create nodes
-            contentTypes.concat(singleTypes).forEach(function (type, i) {
-              var items = entities[i];
-              var name = (0, _lodash.isObject)(type) ? type.name : type;
+          case 25:
+            return _context2.finish(22);
 
-              items.forEach(function (item, i) {
-                var node = (0, _nodes.Node)((0, _lodash.capitalize)(name), item);
-                // Adding new created nodes in an Array
-                newNodes.push(node);
+          case 26:
+            return _context2.finish(19);
 
-                // Create nodes
-                createNode(node);
-              });
-            });
-
-            // Make a diff array between existing nodes and new ones
-            diff = existingNodes.filter(function (_ref4) {
-              var id1 = _ref4.id;
-              return !newNodes.some(function (_ref5) {
-                var id2 = _ref5.id;
-                return id2 === id1;
-              });
-            });
-
-            // Delete diff nodes
-
-            diff.forEach(function (data) {
-              deleteNode({ node: getNode(data.id) });
-            });
-
-            fetchActivity.end();
-
-          case 21:
+          case 27:
           case 'end':
-            return _context.stop();
+            return _context2.stop();
         }
       }
-    }, _callee, undefined);
+    }, _callee, undefined, [[5, 15, 19, 27], [20,, 22, 26]]);
   }));
 
   return function (_x, _x2) {
--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -7,99 +7,95 @@ import authentication from './authentication'
 
 exports.sourceNodes = async (
   { store, actions, cache, reporter, getNode, getNodes },
-  {
-    apiURL = 'http://localhost:1337',
-    contentTypes = [],
-    singleTypes = [],
-    loginData = {},
-    queryLimit = 100,
-  }
+  { sources = {} }
 ) => {
   const { createNode, deleteNode, touchNode } = actions
 
-  // Authentication function
-  let jwtToken = await authentication({ loginData, reporter, apiURL })
+  // The for loop is needed because of the await promises
+  for (const source of sources) {
+    const { apiURL, contentTypes, singleTypes, loginData, queryLimit } = source
 
-  // Start activity, Strapi data fetching
-  const fetchActivity = reporter.activityTimer(`Fetched Strapi Data`)
-  fetchActivity.start()
+    // Authentication function
+    const jwtToken = await authentication({ loginData, reporter, apiURL })
 
-  // Generate a list of promises based on the `contentTypes` option.
-  const contentTypePromises = contentTypes.map(contentType =>
-    fetchData({
+    // Start activity, Strapi data fetching
+    const fetchActivity = reporter.activityTimer(`Fetched Strapi Data`)
+    fetchActivity.start()
+
+    // Generate a list of promises based on the `contentTypes` option.
+    const contentTypePromises = contentTypes.map(contentType =>
+      fetchData({
+        apiURL,
+        contentType,
+        jwtToken,
+        queryLimit,
+        reporter,
+      })
+    )
+
+    // Generate a list of promises based on the `singleTypes` option.
+    const singleTypePromises = singleTypes.map(singleType =>
+      fetchData({
+        apiURL,
+        singleType,
+        jwtToken,
+        queryLimit,
+        reporter,
+      })
+    )
+
+    // Execute the promises
+    let entities = await Promise.all([
+      ...contentTypePromises,
+      ...singleTypePromises,
+    ])
+
+    // Creating files
+    entities = await normalize.downloadMediaFiles({
+      entities,
       apiURL,
-      contentType,
+      store,
+      cache,
+      createNode,
+      touchNode,
       jwtToken,
-      queryLimit,
-      reporter,
     })
-  )
 
-  // Generate a list of promises based on the `singleTypes` option.
-  const singleTypePromises = singleTypes.map(singleType =>
-    fetchData({
-      apiURL,
-      singleType,
-      jwtToken,
-      queryLimit,
-      reporter,
+    // new created nodes
+    let newNodes = []
+
+    // Fetch existing strapi nodes
+    const existingNodes = getNodes().filter(
+      n => n.internal.owner === `gatsby-source-strapi`
+    )
+
+    // Touch each one of them
+    existingNodes.forEach(n => {
+      touchNode({ nodeId: n.id })
     })
-  )
 
-  // Execute the promises
-  let entities = await Promise.all([
-    ...contentTypePromises,
-    ...singleTypePromises,
-  ])
-
-  // Creating files
-  entities = await normalize.downloadMediaFiles({
-    entities,
-    apiURL,
-    store,
-    cache,
-    createNode,
-    touchNode,
-    jwtToken,
-  })
-
-  // new created nodes
-  let newNodes = []
-
-  // Fetch existing strapi nodes
-  const existingNodes = getNodes().filter(
-    n => n.internal.owner === `gatsby-source-strapi`
-  )
-
-  // Touch each one of them
-  existingNodes.forEach(n => {
-    touchNode({ nodeId: n.id })
-  })
-
-  // Merge single and content types and retrieve create nodes
-  contentTypes.concat(singleTypes).forEach((type, i) => {
-    const items = entities[i]
-    const name = isObject(type) ? type.name : type
-
-    items.forEach((item, i) => {
-      const node = Node(capitalize(name), item)
-      // Adding new created nodes in an Array
-      newNodes.push(node)
-
-      // Create nodes
-      createNode(node)
+    // Merge single and content types and retrieve create nodes
+    contentTypes.concat(singleTypes).forEach((type, i) => {
+      const items = entities[i]
+      const name = isObject(type) ? type.name : type
+      items.forEach((item, i) => {
+        const node = Node(capitalize(name), item)
+        // Adding new created nodes in an Array
+        newNodes.push(node)
+        // Create nodes
+        createNode(node)
+      })
     })
-  })
 
-  // Make a diff array between existing nodes and new ones
-  const diff = existingNodes.filter(
-    ({ id: id1 }) => !newNodes.some(({ id: id2 }) => id2 === id1)
-  )
+    // Make a diff array between existing nodes and new ones
+    const diff = existingNodes.filter(
+      ({ id: id1 }) => !newNodes.some(({ id: id2 }) => id2 === id1)
+    )
 
-  // Delete diff nodes
-  diff.forEach(data => {
-    deleteNode({ node: getNode(data.id) })
-  })
-
-  fetchActivity.end()
+    // Delete diff nodes
+    diff.forEach(data => {
+      deleteNode({ node: getNode(data.id) })
+    })
+    fetchActivity.end()
+  }
 }
